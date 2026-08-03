c6fa226d6266181d0089ebde4cbdaefed62b1aec
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-08-03 18:52:11 +0530

    Update Main.java

--- Digital Library Management System/src/main/java/com/library/main/Main.java
+++ Digital Library Management System/src/main/java/com/library/main/Main.java
@@ -1,7 +1,13 @@
 package com.library.main;
 
+import com.library.ui.LoginFrame;
+
 public class Main {
- public static void main(String[]args) {
-	 System.out.println("Welcome to library management system");
-	 }
+
+    public static void main(String[] args) {
+
+        new LoginFrame();
+
+    }
+
 }
